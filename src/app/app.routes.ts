03d66b263c4c589f687570d5b85581e2238cb492
Bruno Costa <bruno.costa@inphonex.com>
2024-08-23 17:52:30 -0300

    update all components for angular 18 and add lazyloading for path home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./home/home.routes').then((routes) => routes.routes),
+  },
   {
     path: 'favorites',
     loadChildren: () =>
