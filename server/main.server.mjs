0d60de99f4b80a7de0ad18439e4c49962f44d05d
Bruno Costa <bruno.costa@inphonex.com>
2024-08-23 18:07:48 -0300

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-USIVSZWH.mjs";import"./chunk-K2TFQLWD.mjs";import"./chunk-K5SMO4WL.mjs";import"./chunk-2CW2GWE3.mjs";import"./chunk-UTEJNEE6.mjs";import"./chunk-ZI4BLURS.mjs";import"./chunk-AQAQSS3K.mjs";import"./chunk-4FYE5D3W.mjs";import"./chunk-X4QNDFMU.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
